e65c2a6ea599abc2ef55d4510ee593b4392ce5f3
Özgür Öğdem <ogdemster@gmail.com>
2022-10-18 12:19:31 +0300

    Faucet Needed

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_KEY = process.env.API_KEY;
+
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    optimism: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      optimism: API_KEY,
+    },
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+const { eth } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  //getContractFactory ile contractımızı değişkene atıyoruz.
+  const creatorhomeNFT = await ethers.getContractFactory("CreatorhomeNFT");
+
+  // deploy the myNftContract dont forget to add your Pinata.Cloud Metadata CID
+  const creatorhomeNFTontract = await creatorhomeNFT.deploy(
+    "ipfs//QmWtgBeokCyezcx7fmm6d8ipoEVuv7ZA2CUEJJAYSr7upk/"
+  );
+
+  //wait for dmyNftContract deployed
+  await creatorhomeNFTontract.deployed();
+
+  // show contract address
+  console.log("Contract address is:", creatorhomeNFTontract.address);
+
+  console.log("Wait for verify");
+  await waitForVerify(100000);
+
+  //dont forget to add your Pinata.Cloud Metadata CID
+  await hre.run("verify:verify", {
+    address: creatorhomeNFTontract.address,
+    constructorArguments: [
+      "ipfs//QmWtgBeokCyezcx7fmm6d8ipoEVuv7ZA2CUEJJAYSr7upk/",
+    ],
+  });
+}
+
+function waitForVerify(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
